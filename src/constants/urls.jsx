d4f589119b361f76411ef8becbe84e01e756983b
foysalahmedmin <foysalahmedmin@gmail.com>
2025-03-19 12:41:18 +0600

    update base url 2

--- src/constants/urls.jsx
+++ src/constants/urls.jsx
@@ -2,8 +2,7 @@ const URLS = {
   baseURL: "https://b1.fifo-tech.com",
   baseMediaURL: "http://bandhan.fifo-tech.com/media/",
   incentiveMediaURL: "http://bandhan.fifo-tech.com/media/incentives/",
-  baseURL_Server2: "http://43.231.78.202:5000",
-  // baseURL_Server2: "http://43.231.78.202:5000",
+  baseURL_Server2: "https://b2.fifo-tech.com",
 };
 
 export default URLS;
