1f18b3d02f283c4e42a510709bec2d483b91c707
foysalahmedmin <foysalahmedmin@gmail.com>
2025-04-15 15:09:54 +0600

    base url

--- src/constants/urls.jsx
+++ src/constants/urls.jsx
@@ -1,7 +1,7 @@
 const URLS = {
   // baseURL: "https://b1.fifo-tech.com",
-  baseURL: "http://localhost:3000",
-  // baseURL: "https://bandhan.fifo-tech.com",
+  // baseURL: "http://localhost:3000",
+  baseURL: "https://bandhan.fifo-tech.com",
   baseMediaURL: "https://bandhan.fifo-tech.com/media/",
   incentiveMediaURL: "https://bandhan.fifo-tech.com/media/incentives/",
   baseURL_Server2: "https://b2.fifo-tech.com",
